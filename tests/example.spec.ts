4a291d73c625ff82ad1b18da25e0e1d8082cdd10
snoopy910 <snoopy5473cool@gmail.com>
2024-04-05 16:14:28 +0900

    test: change the test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -22,12 +22,12 @@
 import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
-	await page.goto("https://playwright.dev/");
-	await page.getByRole("button", { name: "Node.js" }).click();
-	await page
-		.getByLabel("Main", { exact: true })
-		.getByRole("link", { name: "Python" })
-		.click();
-	await page.getByRole("link", { name: "JavaScript" }).click();
-	await page.getByRole("tab", { name: "pnpm" }).click();
+  await page.goto("https://playwright.dev/");
+  await page.getByRole("button", { name: "Node.j" }).click();
+  await page
+    .getByLabel("Main", { exact: true })
+    .getByRole("link", { name: "Python" })
+    .click();
+  await page.getByRole("link", { name: "JavaScript" }).click();
+  await page.getByRole("tab", { name: "pnpm" }).click();
 });
